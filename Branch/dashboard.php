a01fad403c860829e28b4633584e122518b756f7
Sushant Adhikari <sushantadhikari70@gmail.com>
2025-12-04 15:27:42 +0545

    Added code to take measurement from items page

--- Branch/dashboard.php
+++ Branch/dashboard.php
@@ -121,10 +121,7 @@ if (isset($_POST['select_school'])) {
 
             <div style="margin-top:40px; text-align:center;">
                 <a href="advance_payment.php" class="logout-btn" style="background:#667eea; margin-right:15px; padding:12px 25px;">
-                    Advance Collection
-                </a>
-                <a href="measurement.php" class="logout-btn" style="background:#667eea; margin-right:15px; padding:12px 25px;">
-                    Take Measurement
+                    Clothe Collection
                 </a>
                 <a href="logout.php" class="logout-btn" onclick="return confirm('Are you sure you want to logout?')">
                     Logout
